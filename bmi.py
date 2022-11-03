fc7b2ff505e64e7a4904134e69ff7fcb3777fbf5
Firman Shah <firmanshah.21@ichat.sp.edu.sg>
2022-11-03 12:30:24 +0800

    BMI added

---
+++ bmi.py
@@ -0,0 +1,17 @@
+def calculate_bmi(height, weight):
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
+
+    bmi=weight/(height*height)
+    if (bmi < 18.5):{
+        print("You are underweight")
+    }
+    elif (bmi > 25):{
+        print("You are overweight")
+    }
+    else :{
+        print("You are normal")
+    }
+
+
+calculate_bmi(weight=57, height=1.73)
