1e8269ae6290a466d099af46e230b43d5133ea75
Firman Shah <firmanshah.21@ichat.sp.edu.sg>
2022-11-04 16:19:14 +0800

    lol

--- bmi.py
+++ bmi.py
@@ -5,13 +5,12 @@ def calculate_bmi(height, weight):
     bmi=weight/(height*height)
     if (bmi < 18.5):{
         print("You are underweight")
-    }
+    } 
     elif (bmi > 25):{
         print("You are overweight")
     }
     else :{
         print("You are normal")
     }
-
 
 calculate_bmi(weight=57, height=1.73)
